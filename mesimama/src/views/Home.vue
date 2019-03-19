03fd787e14c037dc81e7a1f6e52521971481c3aa
unknown <amihaidany@gmail.com>
2019-03-19 19:08:11 +0200

    Cleaned default CLI project, starting fresh

--- mesimama/src/views/Home.vue
+++ mesimama/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+        <header>
+            header
+        </header>
+        <main>
+            main
+        </main>
+        <footer>
+            footer
+        </footer>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
