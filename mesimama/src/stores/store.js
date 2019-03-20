65a4ac9f9bbb645229f65473688493da479eab89
unknown <amihaidany@gmail.com>
2019-03-20 21:03:11 +0200

    Fix

--- mesimama/src/stores/store.js
+++ mesimama/src/stores/store.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import storeTasks from './modules/store-tasks.js'
 import taskService from '../services/task-service.js'
-
 Vue.use(Vuex)
-
 export default new Vuex.Store({
   modules: {
     // storeTasks
@@ -14,8 +12,6 @@ export default new Vuex.Store({
     filterBy: {},
     currTask: null,
     user: { name: 'puki', _id: 'j3F4fd' }, // TODO: Get the current user from session
-    notificationsCount: 1
- 
   },
   mutations: {
     setTaskItems(state, { tasks }) {
@@ -23,7 +19,6 @@ export default new Vuex.Store({
       state.taskItems = tasks
       console.log(state.taskItems)
     },
- 
     setTaskOwner(state, { taskId }) {
       console.log('inside mutation', state.taskItems)
       console.log('got task id', taskId)
@@ -35,34 +30,24 @@ export default new Vuex.Store({
   },
   actions: {
     loadUnownedTasks(context) {
- 
-  },
-  actions: {
-    loadTaskItems(context) {
       taskService.query()
         .then(tasks => {
           console.log('store got from util', tasks)
           context.commit({ type: 'setTaskItems', tasks })
         })
     },
- 
     async setTaskOwner(context, taskId) {
       await taskService.ownTask(taskId, context.state.user._id)
       context.commit({ type: 'setTaskOwner', taskId })
       console.log('task is owned')
     }
- 
   },
   getters: {
     filteredTasks(state) {
       return state.taskItems
     },
- 
     currUserId(state) {
       return state.user._id
-    },
-    notificationsCount(state) {
-      return state.notificationsCount
     }
   }
 })
