e60bef8fc5ddcc8f8e10c25ece285b9a3f926120
Shai Lavie <shai.lavie@gmail.com>
2019-03-20 17:54:40 +0200

    task preview

--- mesimama/src/components/taskPreview.vue
+++ mesimama/src/components/taskPreview.vue
@@ -9,14 +9,33 @@
         <h4>{{task.dueAt | moment("dddd, MMMM Do YYYY")}}</h4>
         <h4>{{task.status}}</h4>
       </div>
+      <button @click="ownTask(task._id)">Own this Task</button>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  props: ['task'], 
+  props: ['task'],
+  methods: {
+    ownTask(taskId){
+      console.log('tasl is being taken',taskId)
+      this.$emit('task-own',taskId)
+    }
+  },
 }
 </script>
 
- 
+ <style scoped lang="scss">
+button {
+  width: 100px;
+  height: 40px;
+  background-color: rebeccapurple;
+  color: white;
+  border-radius: 8px;
+  transition: .3s ease;
+}
+button:hover {
+  background-color: rgb(119, 82, 156);
+}
+ </style>
