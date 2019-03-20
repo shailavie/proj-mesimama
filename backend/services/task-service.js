e6294b59ce02651be79da4bd5184bedace5b329c
unknown <amihaidany@gmail.com>
2019-03-20 20:29:46 +0200

    Ok

---
+++ backend/services/task-service.js
@@ -0,0 +1,11 @@
+const mongoService = require('./mongo-service')
+const ObjectId = require('mongodb').ObjectId;
+
+function query(creatorId) {
+    return mongoService.connect()
+        .then(db => db.collection('tasks').find({ creatorId }).toArray())
+}
+
+module.exports = {
+    query
+}
