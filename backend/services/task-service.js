6da15fa4a7206608be3f8c66113a5c39d10852ae
unknown <amihaidany@gmail.com>
2019-03-22 13:12:05 +0200

    Ok

--- backend/services/task-service.js
+++ backend/services/task-service.js
@@ -5,9 +5,16 @@ const ObjectId = require('mongodb').ObjectId;
 function query(directorId) {
     let _id = new ObjectId(directorId)
     return mongoService.connect()
-        .then(db => db.collection('tasks').find({ directorId}).toArray())
+        .then(db => db.collection('tasks').find({ directorId }).toArray())
+}
+
+function getById(taskId) {
+    let _id = new ObjectId(taskId)
+    return mongoService.connect()
+        .then(db => db.collection('tasks').findOne({ _id }))
 }
 
 module.exports = {
-    query
+    query,
+    getById
 }
