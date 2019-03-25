880ee1287d578e286477a4c59ce8b69993e635ae
Shai Lavie <shai.lavie@gmail.com>
2019-03-25 13:01:42 +0200

    new task cards

--- frontend/src/views/task-edit.vue
+++ frontend/src/views/task-edit.vue
@@ -1,22 +1,19 @@
 <template>
   <section v-if="taskToEdit" class="task-edit-page">
     <div class="task-edit-container">
-      <!-- <div class="task-img-container">
-        <img :src="user.imgSrc">
-      </div>-->
       <div class="task-edit-form-container">
         <el-form @submit.native.prevent="saveTask" :model="taskToEdit" label-width="100px">
           <el-form-item label="Title">
-            <el-input type="text" v-model="taskToEdit.title"></el-input>
+            <el-input type="text" :maxlength="25" v-model="taskToEdit.title"></el-input>
           </el-form-item>
           <el-form-item label="Description">
-            <el-input type="textarea" v-model="taskToEdit.desc"></el-input>
+            <el-input rows="3" type="textarea" v-model="taskToEdit.desc"></el-input>
           </el-form-item>
-          <el-form-item label="Points">
-            <el-input type="number" v-model="taskToEdit.points"></el-input>
+          <el-form-item label="Task Points">
+            <el-slider v-model="taskToEdit.points" :min="1" :max="3" show-stops></el-slider>
           </el-form-item>
           <el-form-item label="Urgent">
-            <el-switch type="checkbox" v-model="taskToEdit.isUrgent"></el-switch>
+            <el-switch type="checkbox" active-color="#f45642" v-model="taskToEdit.isUrgent"></el-switch>
           </el-form-item>
           <el-form-item label="Due to">
             <el-date-picker
@@ -62,7 +59,6 @@ export default {
   },
   async created() {
     let taskId = this.$route.params.taskId;
-    console.log("taskId", taskId);
     if (taskId) {
       let taskToEdit = await this.$store.dispatch({
         type: "loadTask",
@@ -71,13 +67,14 @@ export default {
       this.taskToEdit = utilService.deepCopy(taskToEdit);
     } else {
       this.taskToEdit = taskService.getEmptyTask();
+      this.taskToEdit.dueAt = this.sameHourTommorrow();
     }
   },
   methods: {
     saveTask() {
       if (!this.taskToEdit._id) this.taskToEdit.createdAt = Date.now();
       this.taskToEdit.directorId = this.$store.getters.currDirectorId;
-      this.taskToEdit.points = Number(this.taskToEdit.points)
+      this.taskToEdit.points = Number(this.taskToEdit.points);
       this.$store.dispatch("saveTask", this.taskToEdit).then(savedTask => {
         this.$store.dispatch({ type: "loadActiveTasks" }).then(() => {
           this.$router.push("/app/tasks");
@@ -85,12 +82,15 @@ export default {
       });
     },
     removeTask() {
-      console.log("DELETE!", this.taskToEdit._id);
       this.$store.dispatch("removeTask", this.taskToEdit._id).then(() => {
         this.$router.push("/app/tasks");
       });
+    },
+    sameHourTommorrow() {
+      return Date.now() + 1000 * 60 * 60 * 24;
     }
-  }
+  },
+  computed: {}
 };
 </script>
 
