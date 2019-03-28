03dba39d136d7cdcc0fb2abf4185abd12e36317d
unknown <amihaidany@gmail.com>
2019-03-28 13:52:11 +0200

    Commit

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,32 +1,33 @@
 <template>
   <section class="login">
-    <div class="login-page-container">
-      <div class="login-box-container">
-        <div class="login-upper-box">
-          <h1>
-            Assist. Collaborate.
-            <br>Get it done.
-          </h1>
-          <!-- <h2 class="cta">{{loginSignupCTA}}</h2> -->
-          <span class="is-member-call">{{loginSignupMsg}}</span>
-          <a @click="isMember=!isMember">{{loginSignupSwitch}}</a>
-          <!-- <button class="login-btn login-btn--facebook">{{loginSignupCTA}} with Facebook</button> -->
-          <div class="login-form">
-            <el-input
-              placeholder="Enter your email address"
-              type="email"
-              autofocus
-              prefix-icon="el-icon-message"
-              v-model="input.email"
-              class="login-page-el-input"
-            ></el-input>
-            <el-button type="primary" class="sign-up-btn">{{loginSignupCTA}}</el-button>
-          </div>
-        </div>
-        <div class="login-middle-box"></div>
-        <div class="login-bottom-box">
-          <div>
-            <div class="qa-box">
+    <h1>Welcome to the Family.</h1>
+    <div class="choose-user">
+      <div class="user-box">
+        <span
+          style="background-image:url('https://scontent.ftlv6-1.fna.fbcdn.net/v/t1.0-9/40515553_10216085148102132_1645739650016346112_o.jpg?_nc_cat=105&_nc_ht=scontent.ftlv6-1.fna&oh=a03924aa5cf182d0ead07372a25e4ca6&oe=5D121E1B');"
+          @click.prevent="setRole('5c98fa5eb687d600001a8d83')"
+          class="login-avatar"
+        ></span>
+        <span>Tamar</span>
+      </div>
+      <div class="user-box">
+        <span
+          style="background-image:url('https://scontent.ftlv6-1.fna.fbcdn.net/v/t1.0-9/14333644_10154601159832375_66942999029735933_n.jpg?_nc_cat=109&_nc_ht=scontent.ftlv6-1.fna&oh=6fd6dde4ef4b2b68c9a03bc16cc1df34&oe=5D16060D');"
+          @click.prevent="setRole('5c98fb581c9d4400002a2a3d')"
+          class="login-avatar"
+        ></span>
+        <span>Ruti</span>
+      </div>
+      <div class="user-box">
+        <span
+          style="background-image:url('https://scontent.ftlv6-1.fna.fbcdn.net/v/t31.0-8/16797654_10211254147530137_4861005975568511724_o.jpg?_nc_cat=106&_nc_ht=scontent.ftlv6-1.fna&oh=9411dd7bd166dfa7a244f436c785983d&oe=5D127B09');"
+          @click.prevent="setRole('5c98fad51c9d4400002a2a3c')"
+          class="login-avatar"
+        ></span>
+        <span>Yonatan</span>
+      </div>
+    </div>
+    <!-- <div class="qa-box">
               <div style="margin-bottom:5px">
                 <div class="curr-user-info" v-if="currUser">
                   <user-avatar :url="currUser.avatarUrl"/>
@@ -41,23 +42,13 @@
                 <el-option value="5c98fad51c9d4400002a2a3c">Yonatan</el-option>
               </el-select>
               <el-button @click="setRole" class="set-role-btn">Set Role</el-button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="login-banner-container"></div>
-    </div>
+    </div>-->
   </section>
 </template>
 
 <script>
-import Axios from "axios";
 import userAvatar from "@/components/user-avatar-cmp.vue";
 import userService from "../services/user.service.js";
-
-var axios = Axios.create({
-  withCredentials: true
-});
 
 export default {
   components: {
@@ -89,9 +80,9 @@ export default {
     }
   },
   methods: {
-    setRole() {
+    setRole(id) {
       userService
-        .setUserSession(this.role)
+        .setUserSession(id)
         .then(res => {
           console.log("Session is ", res);
         })
@@ -114,6 +105,15 @@ export default {
 
 
 <style lang="scss" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: linear-gradient(45deg, #296bbb 1%, #1e88e5 64%, #279ad4 97%);
+  color: #fff;
+  flex-direction: column;
+}
 a {
   cursor: pointer;
 }
@@ -180,6 +180,29 @@ h1 {
 }
 
 .login-page-el-input .el-input__inner {
+}
+
+.choose-user {
+  margin-top: 60px;
+  display: flex;
+  width: 500px;
+  justify-content: space-between;
+}
+
+.login-avatar {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-size: cover;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+
+.user-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
 
--- frontend/src/views/tasks.vue
+++ frontend/src/views/tasks.vue
@@ -60,7 +60,7 @@
 
     <section class="stats-panel">
       <dash-board></dash-board>
-      <!-- <podium-board-cmp></podium-board-cmp> -->
+      <podium-board-cmp></podium-board-cmp>
       <photo-gallery class="photo-gallery"/>
     </section>
   </section>
